75da06bbb24d2fb27e886eaec79c7000963ec8a0
Cemal Balaban <cemal.balaban1@gmail.com>
2021-06-23 13:28:46 +0300

    Index-Login-Register ön yüz yazıldı.

--- Emlak_Yorumlari/Emlak_Yorumlari_WebApp/Controllers/HomeController.cs
+++ Emlak_Yorumlari/Emlak_Yorumlari_WebApp/Controllers/HomeController.cs
@@ -13,5 +13,39 @@ namespace Emlak_Yorumlari_WebApp.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /*[HttpPost] //post işlemi yapılmadı
+        public ActionResult Login()
+        {
+            return View();
+        }*/
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        /*[HttpPost] // post işlemi yapılmadı
+        public ActionResult Register()
+        {
+            return View();
+        }*/
+        public ActionResult UserActivate()
+        {
+            //aktivasyon işlemi
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            //çıkış yapma
+            return View();
+        }
     }
 }
